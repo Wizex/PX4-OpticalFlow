a0f1dd0762aa900743bf4d794445d1db3f815053
Oleh Treskot <treskot.oleg@gmail.com>
2025-12-28 20:08:55 +0200

    Return raw pixel flow

--- src/flow_px4.cpp
+++ src/flow_px4.cpp
@@ -32,20 +32,20 @@
  ****************************************************************************/
 
 /*
-*  flow_px4.cpp
-*
-*  Created on: Dec 13, 2016
-*      Author: Christoph
-*
-****************************************************************************
-* PX4Flow flow calculation
-****************************************************************************/
+ *  flow_px4.cpp
+ *
+ *  Created on: Dec 13, 2016
+ *      Author: Christoph
+ *
+ ****************************************************************************
+ * PX4Flow flow calculation
+ ****************************************************************************/
 
 #include "flow_px4.hpp"
 #include <iostream>
 
 OpticalFlowPX4::OpticalFlowPX4(float f_length_x, float f_length_y, int ouput_rate, int img_width, int img_height,
-			       int search_size, int flow_feature_threshold, int flow_value_threshold)
+															 int search_size, int flow_feature_threshold, int flow_value_threshold)
 {
 	setImageWidth(img_width);
 	setImageHeight(img_height);
@@ -55,7 +55,7 @@ OpticalFlowPX4::OpticalFlowPX4(float f_length_x, float f_length_y, int ouput_rat
 
 	initLimitRate();
 
-	//init the PX4Flow instance
+	// init the PX4Flow instance
 	px4_flow = new PX4Flow(img_width, search_size, flow_feature_threshold, flow_value_threshold);
 	initialized = false;
 	img_old = new uint8_t[image_width * image_height];
@@ -63,36 +63,29 @@ OpticalFlowPX4::OpticalFlowPX4(float f_length_x, float f_length_y, int ouput_rat
 
 OpticalFlowPX4::~OpticalFlowPX4(void)
 {
-
 }
 
 int OpticalFlowPX4::calcFlow(uint8_t *img_current, const uint32_t &img_time_us, int &dt_us, float &flow_x,
-			     float &flow_y)
+														 float &flow_y)
 {
 
-	if (!initialized) {
-		//first call of the function -> copy image for flow calculation
+	if (!initialized)
+	{
+		// first call of the function -> copy image for flow calculation
 		memcpy(img_old, img_current, image_width * image_height * sizeof(uint8_t));
 		initialized = true;
 		return 0;
 	}
 
-	//not needed
+	// not needed
 	float x_gyro_rate = 0;
 	float y_gyro_rate = 0;
 	float z_gyro_rate = 0;
 
 	int flow_quality = px4_flow->compute_flow(img_old, img_current,
-			   x_gyro_rate, y_gyro_rate, z_gyro_rate, &flow_x, &flow_y);
+																						x_gyro_rate, y_gyro_rate, z_gyro_rate, &flow_x, &flow_y);
 
 	memcpy(img_old, img_current, image_width * image_height * sizeof(uint8_t));
 
-	flow_quality = limitRate(flow_quality, img_time_us, &dt_us, &flow_x, &flow_y);
-
-	flow_x = atan2(flow_x, focal_length_x); //convert pixel flow to angular flow
-	flow_y = atan2(flow_y, focal_length_y); //convert pixel flow to angular flow
-
-	return flow_quality;
-
-
+	return limitRate(flow_quality, img_time_us, &dt_us, &flow_x, &flow_y);
 }
